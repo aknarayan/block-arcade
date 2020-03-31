ee9b98b5d1d1527d6c01716675c6c172024cd314
akman5599 <akshay.narayan5599@gmail.com>
2020-03-31 20:34:20 +0100

    Implemented basic player jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,16 @@ public class PlayerMovement : MonoBehaviour {
   public Rigidbody rb;
   public float forwardForce = 2000f;
   public float sidewaysForce = 500f;
+  public float upwardsForce = 5f;
+  private bool isGrounded;
+
+  private void OnCollisionStay(Collision collision) {
+    isGrounded = true;
+  }
+
+  private void OnCollisionExit(Collision collision) {
+    isGrounded = false;
+  }
 
   void FixedUpdate() {
     rb.AddForce(0, 0, forwardForce * Time.deltaTime);
@@ -13,6 +23,9 @@ public class PlayerMovement : MonoBehaviour {
       rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     } else if (Input.GetKey(KeyCode.A)) {
       rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    } else if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+      rb.AddForce(upwardsForce * Vector3.up, ForceMode.Impulse);
+      isGrounded = false;
     }
 
     if (rb.position.y < -1f) {
