07515b1c1be543e0ca7121f08fd1508f6a6a6375
akman5599 <akshay.narayan5599@gmail.com>
2020-03-31 20:06:56 +0100

    Implemented player movement, levels, and collisions.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+  private bool gameHasEnded = false;
+  public float restartDelay = 1.0f;
+  public GameObject completeLevelUI;
+
+  public void CompleteLevel() {
+    completeLevelUI.SetActive(true);
+  }
+
+  public void EndGame() {
+    if (!gameHasEnded) {
+      gameHasEnded = true;
+      Invoke("Restart", restartDelay);
+    }
+  }
+
+  void Restart() {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+  public Rigidbody rb;
+  public float forwardForce = 2000f;
+  public float sidewaysForce = 500f;
+
+  void FixedUpdate() {
+    rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.D)) {
+      rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    } else if (Input.GetKey(KeyCode.A)) {
+      rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    if (rb.position.y < -1f) {
+      FindObjectOfType<GameManager>().EndGame();
+    }
+
+  }
+}
