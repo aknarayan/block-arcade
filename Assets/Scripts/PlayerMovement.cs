009c31123aa0218238720023c5c79b9bac614366
akman5599 <akshay.narayan5599@gmail.com>
2020-03-31 20:26:09 +0100

    Revert "Implemented basic player jump functionality"
    This reverts commit e99766b2352a39a7a330d6ebcd8bed36fe46498c.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,6 @@ public class PlayerMovement : MonoBehaviour {
   public Rigidbody rb;
   public float forwardForce = 2000f;
   public float sidewaysForce = 500f;
-  public float upwardsForce = 10f;
-  private bool isGrounded;
-
-  private void OnCollisionStay(Collision collision) {
-    isGrounded = true;
-  }
-
-  private void OnCollisionExit(Collision collision) {
-    isGrounded = false;
-  }
 
   void FixedUpdate() {
     rb.AddForce(0, 0, forwardForce * Time.deltaTime);
@@ -23,8 +13,6 @@ public class PlayerMovement : MonoBehaviour {
       rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     } else if (Input.GetKey(KeyCode.A)) {
       rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-    } else if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-      rb.AddForce(upwardsForce * Vector3.up, ForceMode.Impulse);
     }
 
     if (rb.position.y < -1f) {
