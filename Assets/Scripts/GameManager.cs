dd11c2f8c57e9f822872e4977708caf669f23ec5
akman5599 <akshay.narayan5599@gmail.com>
2020-04-01 22:26:38 +0100

    Implemented pause/play features

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
   private bool gameHasEnded = false;
   public float restartDelay = 1.0f;
   public GameObject completeLevelUI;
+  public Button pauseButton;
+  public Button playButton;
 
   public void CompleteLevel() {
     completeLevelUI.SetActive(true);
@@ -21,4 +24,17 @@ public class GameManager : MonoBehaviour {
   private void Restart() {
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
+
+  public void PauseGame() {
+    pauseButton.gameObject.SetActive(false);
+    playButton.gameObject.SetActive(true);
+    Time.timeScale = 0;
+  }
+
+  public void ResumeGame() {
+    pauseButton.gameObject.SetActive(true);
+    playButton.gameObject.SetActive(false);
+    Time.timeScale = 1;
+  }
+
 }
